a095735f01ee3a29bf591a69d0410778af0d5dad
Fatima <44647655+fatima196@users.noreply.github.com>
2020-06-23 17:39:31 +0200

    Update RLE.java

--- RLE.java
+++ RLE.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package jpeg;
 
 import java.io.File;
@@ -14,10 +10,7 @@ import java.util.Scanner;
 import java.util.Vector;
 import sun.misc.FloatingDecimal;
 
-/**
- *
- * @author lenovo
- */
+
 public class RLE {
     
 	public static void main(String[] args) throws IOException {

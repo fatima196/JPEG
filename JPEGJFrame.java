36dc2081b40dfc35fd2d563cdb7fc2215402699b
Fatima <44647655+fatima196@users.noreply.github.com>
2020-06-23 17:37:23 +0200

    Update JPEGJFrame.java

--- JPEGJFrame.java
+++ JPEGJFrame.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package jpeg;
 
 import java.io.File;
@@ -18,10 +14,7 @@ import java.util.logging.Logger;
 import static jpeg.RLE.HuffmanReverse;
 import static jpeg.RLE.getCategory;
 
-/**
- *
- * @author lenovo
- */
+
 public class JPEGJFrame extends javax.swing.JFrame {
 
     /**
